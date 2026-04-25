cb3b8817cb9ae57ddac6b519f0562a1d0b4e8085
Edu <luis_100404@hotmail.com>
2026-04-25 11:36:11 -0300

    new files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import styles from './App.module.css'
+
+export default function App() {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: "url('./background.jpeg')",
+      }}
+    >
+      <div className={styles.overlay}></div>
+
+      <main className={styles.content}>
+        <div className={styles.logo}>
+          <img src="./logo.jpeg" alt="Logo" />
+        </div>
+
+        <h1 className={styles.title}>Revenda Premium</h1>
+        <p className={styles.subtitle}>Atendimento rápido e exclusivo</p>
+
+        <a
+          href="https://wa.me/5581996680762"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.button}
+        >
+          Atendimento Atacado
+        </a>
+      </main>
+    </div>
+  )
+}
